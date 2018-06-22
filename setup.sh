d8f0efdb67d38a4997bf1fe109c326f5761c506e
Sandeep <sandeep_saini@outlook.com>
2018-06-22 17:08:37 +0100

    epic fail

--- setup.sh
+++ setup.sh
@@ -39,6 +39,7 @@ echo "6. Installing Sublime Text 3"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
+sudo apt-get install sublime-text
 
 echo "========================="
 
